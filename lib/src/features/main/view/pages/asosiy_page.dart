553bf5943da3d26b941cf50e38b10a04bc191f30
muxamad <alimjonovmuxammadamin@gmail.com>
2025-08-20 12:17:22 +0500

    Muhammadaminsiddiq asosiy oynani yaratishni boshladi 1 - commit

---
+++ lib/src/features/main/view/pages/asosiy_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AsosiyPage extends StatefulWidget {
+  const AsosiyPage({super.key});
+
+  @override
+  State<AsosiyPage> createState() => _AsosiyPageState();
+}
+
+class _AsosiyPageState extends State<AsosiyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
